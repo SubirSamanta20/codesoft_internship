b5b98bf3b31d43c1489880f8d5a599981ae4302a
Subir_Samanta <145208064+SubirSamanta20@users.noreply.github.com>
2023-10-26 12:18:51 +0530

    Add files via upload

---
+++ pw_generator.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+def generate_password(length):
+   
+    characters = string.ascii_letters + string.digits
+
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
+
+def main():
+    try:
+        
+        length = int(input("Enter the desired password length: "))
+
+        if length <= 0:
+            print("Password length must be a positive integer.")
+        else:
+            password = generate_password(length)
+            print("Generated Password: " + password)
+
+    except ValueError:
+        print("Invalid input. Please enter a valid positive integer for the password length.")
+
+if __name__ == "__main__":
+    main()
